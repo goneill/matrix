109d0b708a2d93b90831af59cb217e766eaa7171
goneill <grainne@gmail.com>
2016-12-27 18:19:30 -0500

    update query

--- getallphone.php
+++ getallphone.php
@@ -18,20 +18,24 @@ if (!$link) {
 
 $phoneNumber = $_GET['number'];
 echo "$phoneNumber<BR>";
-$query = "SELECT PhoneTo.PhoneNumber, PhoneTo.PhoneID, PhoneTo.ShortName, PhoneFrom.PhoneNumber, PhoneFrom.PhoneID, PhoneFrom.ShortName,PhoneCalls.CallToPhoneID, PhoneCalls.CallFromPhoneID, PhoneCalls.StartDate, PhoneCalls.EndDate, PhoneCalls.Duration, PhoneCalls.FirstLatitude, PhoneCalls.FirstLongitude From Phones as PhoneTo, PhoneCalls, Phones as PhoneFrom WHERE (PhoneTo.PhoneNumber='$phoneNumber' OR PhoneFrom.PhoneNumber = '$phoneNumber') AND PhoneTo.PhoneID = PhoneCalls.CallToPhoneID AND PhoneFrom.PhoneID = PhoneCalls.CallFromPhoneID ORDER BY StartDate";
+$query = "SELECT PhoneTo.PhoneNumber as PhoneToPhoneNumber, PhoneTo.PhoneID as PhoneToPhoneID, PhoneTo.ShortName as PhoneToShortName, PhoneFrom.PhoneNumber as PhoneFromPhoneNumber, PhoneFrom.PhoneID as PhoneFromPhoneID, PhoneFrom.ShortName as PhoneFromShortName, PhoneCalls.PhoneCallID, PhoneCalls.StartDate, PhoneCalls.EndDate, PhoneCalls.Duration, PhoneCalls.FirstLatitude, PhoneCalls.FirstLongitude From Phones as PhoneTo, PhoneCalls, Phones as PhoneFrom WHERE (PhoneTo.PhoneNumber='$phoneNumber' OR PhoneFrom.PhoneNumber = '$phoneNumber') AND PhoneTo.PhoneID = PhoneCalls.CallToPhoneID AND PhoneFrom.PhoneID = PhoneCalls.CallFromPhoneID ORDER BY StartDate";
+if ($result = $link->query($query)) {
+	while($row=$result->fetch_array()) {
+		$rows[] = $row;
+	}
+	foreach($rows as $row) {
 
-if (!$result = mysqli_query($link, $query)) {
-	die('Error: ' . mysqli_error($link));
-}
-$calls = $link->query($query);
-$row = $calls->fetch_assoc();
-if ($calls->num_rows === 0 ) {
-	echo "nothing returned <BR> $query <BR>";
+		echo "it worked!";
+		$phoneToPhoneNumber = $row["PhoneToPhoneNumber"];
+		die();
+	}
+	echo "in the if statement - no results...<BR>";
 } else {
-	$startLatitude = $row["Latitude"];
-	$startLongitude = $row["Longitude"];
-	$startAzimuth = 0;
+	echo "stmt didnt work<BR>";
 }
+
+
+
 
 
 ?>
