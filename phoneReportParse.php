c4d034d9309d0fb189397c2ed779149dd8a2f5b0
goneill <grainne@gmail.com>
2017-10-02 08:54:27 -0400

    Trying to update to get the phone report from pdf

--- phoneReportParse.php
+++ phoneReportParse.php
@@ -16,7 +16,17 @@ $txtFile = "output/txtPhoneReport.txt";
 echo "pdfFile: $pdfFile <BR> txtFile: $txtFile <BR>";
 // create an array for all of the thigns that are in this specific report that you want to break into separate output documents (ie for import into xls) - in the future this would be tables in a database but alas we are not there yet!
 
-
+function getSpaceDelimitedFields($string,$field,$numSpaces) {
+	$string = trim($string);
+	echo "string: $string <BR>";
+	$pattern = "/\s{".$numSpaces.",}[^(\s]+\s{".$numSpaces.",}/";
+	echo "pattern $pattern<BR>";
+	preg_match_all($pattern, utf8_decode($string), $matches, PREG_OFFSET_CAPTURE);
+	print_r($matches);
+    $spaceLoc = $matches[0][$field-1][1];
+    echo "<BR>location of the $field space is: $spaceLoc<BR>";
+    return substr($string, 0, $spaceLoc); 
+}
 // the smalot parser for pdf doens't work as well at keepign tables as this command line utility form a milion years ago; it is faster and better for layout
 /*$execstring = "/usr/local/bin/pdftotext  -layout '$pdfFile' '$txtFile'";
 echo "executing: " . $execstring . "\n<BR>";
@@ -38,7 +48,7 @@ foreach ($lines as $line_num => $line) {
 		$pattern = '/\fSMS Messages /';
 			if (preg_match($pattern, $line)) {
 				
-				echo "found the SMS: $line";
+				echo "found the SMS: $line <BR>";
 
 				$status = 'SMS';
 			}
@@ -49,13 +59,12 @@ foreach ($lines as $line_num => $line) {
 			if (preg_match($pattern, $line, $matches)) {
 				$SMSType = trim($matches[0]); // need to trim out the num in parens
 				echo "SMS TYPE: $SMSType . <BR>";
-				die();
 			}
 			$pattern = '/^\s*[0-9]+\s+(From|To)/';
-			if (preg_match($pattern, $line) { // this means that we are generating actual data on this line - a first line of data
-				$contentSMSLine = 1
-				$direction = substr($line, 8, )
-
+			if (preg_match($pattern, $line)) { // this means that we are generating actual data on this line - a first line of data
+				$direction = getSpaceDelimitedFields(trim($line), 2,3);
+				echo "direction: $direction<BR>";
+				die(); 
 			}
 			break;	
 
